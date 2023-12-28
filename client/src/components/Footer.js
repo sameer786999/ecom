dacc713f02f51289aa622d75fad7f04477607732
sameer78699 <sameerdastagir5@gmail.com>
2023-12-28 20:03:23 +0530

    created Homepage

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+
 export default function Footer() {
   return (
     <div>
-      Footer
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div className="col-md-4 d-flex align-items-center">
+          
+          <span className="mb-3 mb-md-0 text-muted">© 2023 LoveDe, Inc</span>
+        </div>
+
+        <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+          
+        </ul>
+      </footer>
     </div>
-  )
+  );
 }
--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,41 +1,23 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Card from '../components/Card';
+import Carousel from '../components/Carousel';
 export default function Home() {
   return (
     <div>
       <Navbar />
       <div>
-        <div
-          className="card mt-3"
-          style={{ width: "18rem", maxHeight: "360px" }}
-        >
-          <img src="" className="card-img-top" alt="" />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">This is My website</p>
-            <div className="container w-100">
-              <select className="mt-2 h-100 bg-success">
-                {Array.from(Array(6), (e, i) => {
-                  return (
-                    <option key={i + 1} value={i + 1}>
-                      {i + 1}
-                    </option>
-                  );
-                })}
-              </select>
-              <select className="m-2 h-100  bg-success rounded">
-                <option value="half">Half</option>
-                <option value="full">Full</option>
-              </select>
-              <div className='d-inline h-100 fs-5 '>Total Price</div>
-
-
-            </div>
-          </div>
-        </div>
+        <Carousel />
       </div>
+
       <div>
+        <div className="m-3">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
         <Footer />
       </div>
     </div>
