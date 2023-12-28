4a07ee22a65568dbf8fca94a685e1be841a3338e
sameer78699 <sameerdastagir5@gmail.com>
2023-12-28 19:04:11 +0530

    git

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import  {Link} from 'react-router-dom'
 export default function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            Navbar
-          </a>
+          <Link className="navbar-brand fs-1 fst-italic " to="/">
+            LoveDe!
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -22,30 +22,17 @@ export default function Navbar() {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <Link className="nav-link active" aria-current="page" to="/">
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Features
-                </a>
+                <Link className="nav-link" to="/login">
+                 Login
+                </Link>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link disabled"
-                  href="/"
-                  tabindex="-1"
-                  aria-disabled="true"
-                >
-                  Disabled
-                </a>
-              </li>
+             
+             
             </ul>
           </div>
         </div>
